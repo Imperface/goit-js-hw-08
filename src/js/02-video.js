9f783d9a0dbd6880478a3d8b59c3f0e133f7c34f
Imperface <kovzhenko.ua@gmail.com>
2023-08-27 01:22:44 +0300

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframeRef = document.querySelector('#vimeo-player');
+
+const player = new Player(iframeRef);
+const CURRENT_TIME_SET = 'videoplayer-current-time';
+
+const timeUpdate = data => {
+  const timeUpdateSeconds = data.seconds;
+  localStorage.setItem(CURRENT_TIME_SET, timeUpdateSeconds);
+};
+const onIframePlay = data => {};
+
+player.setCurrentTime(localStorage.getItem(CURRENT_TIME_SET));
+
+player.on('play', onIframePlay);
+player.on('timeupdate', throttle(timeUpdate, 1000));
+player.off('play', onIframePlay);
