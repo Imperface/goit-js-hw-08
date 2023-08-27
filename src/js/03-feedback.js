ca9a1a53a839391084b15be0a39edd5fffda2a50
Imperface <kovzhenko.ua@gmail.com>
2023-08-28 00:13:58 +0300

    task3 fix submit

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,6 @@
 import throttle from 'lodash.throttle';
 const REVIEW_DATA = 'formData';
 const parsedData = JSON.parse(localStorage.getItem(REVIEW_DATA));
-
 let formData = parsedData ? { ...parsedData } : {};
 const refs = {
   form: document.querySelector('.feedback-form'),
@@ -14,9 +13,8 @@ const checkLocalStorage = () => {
     return;
   }
   refs.input.value = parsedData.email || '';
-  console.log('input updated successfully');
   refs.textArea.value = parsedData.message || '';
-  console.log('textarea updated successfully ');
+  console.log('data updated successfully');
 };
 
 const ofFormSubmit = e => {
@@ -24,7 +22,6 @@ const ofFormSubmit = e => {
   e.preventDefault();
   console.log(formData);
   localStorage.removeItem(REVIEW_DATA);
-  formData = {};
   refs.form.reset();
   console.log('localStorage clearing');
 };
