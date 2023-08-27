5fefee9759790e0e47a87fdc0b2d12d40249b09c
Imperface <kovzhenko.ua@gmail.com>
2023-08-27 23:59:13 +0300

    fix formData error

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,12 +1,13 @@
 import throttle from 'lodash.throttle';
 const REVIEW_DATA = 'formData';
-const formData = {};
+const parsedData = JSON.parse(localStorage.getItem(REVIEW_DATA));
+
+let formData = parsedData ? { ...parsedData } : {};
 const refs = {
   form: document.querySelector('.feedback-form'),
   input: document.querySelector('input'),
   textArea: document.querySelector('textarea'),
 };
-const parsedData = JSON.parse(localStorage.getItem(REVIEW_DATA));
 const checkLocalStorage = () => {
   if (!parsedData) {
     console.log('local storage is empty');
@@ -22,9 +23,8 @@ const ofFormSubmit = e => {
   console.log('submit successfully');
   e.preventDefault();
   console.log(formData);
-  console.log(`user email: ${formData.email}`);
-  console.log(`user message: ${formData.message}`);
   localStorage.removeItem(REVIEW_DATA);
+  formData = {};
   refs.form.reset();
   console.log('localStorage clearing');
 };
